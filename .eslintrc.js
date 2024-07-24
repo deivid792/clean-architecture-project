bf2b3dded64b0edc27b45f5a2d5f7c99cfe7bd01
Deivid <deividcastro792@gmail.com>
2024-07-24 04:42:07 -0300

    config development

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,13 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vasr': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+
+
+
+
   },
 };
