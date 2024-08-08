0f7cccf5313711096685e3e8b334e4ff6b3d124e
Deivid <deividcastro792@gmail.com>
2024-08-08 03:45:48 -0300

    correcting errors

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { EnvConfig } from './env-config.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+  ConfigService: any;
+  constructor( private configService: ConfigService) {}
+  getAppPort(): number {
+    //return Number( value: this.ConfigService.get<number>(propertyPath: 'PORT'))
+    throw new Error('Method not implemented.');
+  }
+  getNodeEnv(): string {
+    throw new Error('Method not implemented.');
+  }
+}
